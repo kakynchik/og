e83161b91f06d1e5ef6fa6aacc269e5635676f80
kakynchik <ukakyn@gmail.com>
2023-05-23 20:08:58 +0300

    pershe zavdannya

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import logging   #стандартна бібліотека для логування перебігу програми
-logging.basicConfig(level=logging.DEBUG,
-                    filename= "logs.log",
-                    filemode= "w",
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug("debug")
-logging.info("info")
-logging.error("error")
-logging.warning("warning")
-logging.critical("critical")
-try:
-    print(10/0)
-except Exception:
-    logging.exception("pomilka")
-#faktorial chisla
+#1
+import logging
 
-def factorial(n):
-    logging.info(f"rozpochato obchislennya factorialu chisna {n}")
-    result = 1
-    for i in range(1, n + 1):
-        result += 1 #1*2*3...
-    logging.info(f"obchislennya factorialy dlya chisla zaversheno {n}. rezultat vikonannya {result}")
-    return result
-
-logging.basicConfig(level=logging.INFO)
-factorial(5)
+class Calc:
+    def __init__(self):
+        self.logger = logging.getLogger("calculator")
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(logging.StreamHandler())
+    def add(self, a, b):
+        result = a + b
+        self.logger.info(f" {a} + {b} = {result}")
+        return result
+    def min(self, a, b):
+        result = a - b
+        self.logger.info(f" {a} - {b} = {result}")
+        return result
+    def mno(self, a, b):
+        result = a * b
+        self.logger.info(f" {a} * {b} = {result}")
+        return result
+    def dil(self, a, b):
+        if b != 0:
+            result = a / b
+            self.logger.info(f" {a} / {b} = {result}")
+            return result
+        else:
+            raise ValueError("pomilka! dilennya na moyu samoocenky!")
+calc = Calc()
+calc.add(6, 5)
+calc.min(123, 123)
+calc.mno(9, 123)
+calc.dil(12, 45)
