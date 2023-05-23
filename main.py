a18d82b1b94bbc3b78c2b3b683942c868dbb3eec
kakynchik <ukakyn@gmail.com>
2023-05-23 20:30:38 +0300

    dryge zavdannya

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-#1
+#2
 import logging
 
-class Calc:
-    def __init__(self):
-        self.logger = logging.getLogger("calculator")
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
-    def add(self, a, b):
-        result = a + b
-        self.logger.info(f" {a} + {b} = {result}")
-        return result
-    def min(self, a, b):
-        result = a - b
-        self.logger.info(f" {a} - {b} = {result}")
-        return result
-    def mno(self, a, b):
-        result = a * b
-        self.logger.info(f" {a} * {b} = {result}")
-        return result
-    def dil(self, a, b):
-        if b != 0:
-            result = a / b
-            self.logger.info(f" {a} / {b} = {result}")
-            return result
-        else:
-            raise ValueError("pomilka! dilennya na moyu samoocenky!")
-calc = Calc()
-calc.add(6, 5)
-calc.min(123, 123)
-calc.mno(9, 123)
-calc.dil(12, 45)
+def write_file(fill_path, data):
+    try:
+        with open(fill_path, 'w') as file:
+            file.write(data)
+        logging.info(f"dani z file {fill_path} zchitano")
+    except Exception as e:
+        logging.error(f'stalas pomilka y {fill_path} file {e}')
+
+logging.basicConfig(level=logging.INFO)
+write_file("output.txt", input("vvedit sho treba vpisaty v file: "))
