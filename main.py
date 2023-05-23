ff0faa4f0e2f8587debb5d1a013b64a83986517d
kakynchik <ukakyn@gmail.com>
2023-05-23 20:54:05 +0300

    3 zavdannya

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-#1
+import random
 import logging
 
-class Calc:
-    def __init__(self):
-        self.logger = logging.getLogger("calculator")
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
-    def add(self, a, b):
-        result = a + b
-        self.logger.info(f" {a} + {b} = {result}")
-        return result
-    def min(self, a, b):
-        result = a - b
-        self.logger.info(f" {a} - {b} = {result}")
-        return result
-    def mno(self, a, b):
-        result = a * b
-        self.logger.info(f" {a} * {b} = {result}")
-        return result
-    def dil(self, a, b):
-        if b != 0:
-            result = a / b
-            self.logger.info(f" {a} / {b} = {result}")
-            return result
-        else:
-            raise ValueError("pomilka! dilennya na moyu samoocenky!")
-calc = Calc()
-calc.add(6, 5)
-calc.min(123, 123)
-calc.mno(9, 123)
-calc.dil(12, 45)
+logging.basicConfig(filename='random_numbers.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+def generate_random_numbers(file_path, count):
+    try:
+        with open(file_path, 'w') as file:
+            for i in range(1, count + 1):
+                random_number = random.randint(1, 100)
+                file.write(f'{i}. {random_number}\n')
+                logging.info(f'Generated random number: {random_number}')
+            logging.info(f'Successfully generated and saved {count} random numbers.')
+    except Exception as e:
+        logging.error(f'Error occurred: {str(e)}')
+
+file_path = 'random_numbers.txt'
+number_count = 10
+
+generate_random_numbers(file_path, number_count)
